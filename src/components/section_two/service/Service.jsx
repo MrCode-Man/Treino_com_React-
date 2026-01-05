ca97c79dfdc73bcb36befdb660d3daad2283e430
MrCode-Man <minimina722@gmail.com>
2026-01-05 18:50:31 -0300

    adicionado a seção de cards e criada a seção do botão abaixo

--- src/components/section_two/service/Service.jsx
+++ src/components/section_two/service/Service.jsx
@@ -1,13 +1,16 @@
 import './Service.css'
+import CardSection from "../cards/CardSection.jsx";
 
 function Service() {
     return (
         <section>
             <div className='main_container'>
                 <h2>Services</h2>
-
-
-
+                <CardSection/>
+                <div className='botao_container'>
+                    <a className='link_botao'>BOOK AN APPOINTMENT</a>
+                    <p>OR DROP BY WHEN WE ARE OPEN</p>
+                </div>
             </div>
         </section>
     )
