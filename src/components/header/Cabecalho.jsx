80d2988d204e8e46164426b5d2fa21c0a3bcaf43
MrCode-Man <minimina722@gmail.com>
2026-01-10 00:28:03 -0300

    acomodei formas com o cabeçalho e arrumei os imports

--- src/components/header/Cabecalho.jsx
+++ src/components/header/Cabecalho.jsx
@@ -1,10 +1,12 @@
 import './Cabecalho.css'
 import design from '../../assets/icons/ideia.svg'
-import LinksHeader from './Navbar.jsx'
+import LinksHeader from './LinksHeader.jsx'
+import Formas from "./Formas.jsx";
 
 function Cabecalho() {
     return (
         <div className='header'>
+            <Formas/>
             <img className='logoSVG' src={design} alt='the design center'></img>
             <LinksHeader/>
         </div>
