cf79be94fe77ef61eed36df5f56efb90af37e382
MrCode-Man <minimina722@gmail.com>
2026-01-13 12:17:13 -0300

    criada a section de cards com horários de funcionamento

--- src/components/section_two/schedule/scheduleCards/ScheduleCards.jsx
+++ src/components/section_two/schedule/scheduleCards/ScheduleCards.jsx
@@ -1,9 +1,20 @@
+import './ScheduleCards.css'
+
 function ScheduleCards() {
     return (
         <ul className="schedule_ul">
-            <li className="schedule_card"></li>
-            <li className="schedule_card"></li>
-            <li className="schedule_card"></li>
+            <li className="schedule_card">
+                <h3>Monday</h3>
+                <p>4-7pm</p>
+            </li>
+            <li className="schedule_card">
+                <h3>Wednesday</h3>
+                <p>1-3pm <br></br> 4-7pm</p>
+            </li>
+            <li className="schedule_card">
+                <h3>Thursday</h3>
+                <p>1-7pm</p>
+            </li>
         </ul>
     )
 }
