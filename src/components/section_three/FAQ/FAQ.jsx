e8ea0f5c6ee16691348ba7b4e9b4dd7e86838cf7
MrCode-Man <minimina722@gmail.com>
2026-01-18 09:13:29 -0300

    usei 'useState' para alterar classes css's para conseguir manipular melhor no css

--- src/components/section_three/FAQ/FAQ.jsx
+++ src/components/section_three/FAQ/FAQ.jsx
@@ -1,13 +1,22 @@
 import './FAQ.css'
+import {useState} from "react";
 
 function FAQ({titulo, descricao}){
 
+    const [classeDisplay, setClasseDisplay] = useState('esconde');
+
+    function mudaDisplay(){
+        setClasseDisplay( prev=> (prev === 'esconde' ? 'mostra' : 'esconde'));
+    }
+
+    const isSelecionado = classeDisplay === 'mostra';
+
     return (
-        <div className='FAQ_container'>
-            <div className='sumario'>
+        <div className={`FAQ_container ${isSelecionado ? 'selecionado' : ''}`}>
+            <div className='sumario' onClick={mudaDisplay}>
                 <h3 className='titulo_FAQ'>{titulo}</h3>
             </div>
-            <p className='descricao'> {descricao}</p>
+            <p className={classeDisplay}> {descricao}</p>
         </div>
     )
 }
