469850636f49adf0eb1ad9f7d5e27c0e2428db27
MrCode-Man <minimina722@gmail.com>
2026-01-21 11:18:26 -0300

    formatei os imports para a organização que eu gosto; mudei de div para section o container geral

--- src/components/section_two/SectionTwo.jsx
+++ src/components/section_two/SectionTwo.jsx
@@ -1,15 +1,15 @@
 import './SectionTwo.css'
 import MainText from "./MainText.jsx";
-import Service from "./service/Service.jsx";
+import Service  from "./service/Service.jsx";
 import Schedule from "./schedule/Schedule.jsx";
 
 function SectionTwo() {
     return (
-        <div className="SectionTwo">
+        <section className="SectionTwo">
             <MainText/>
             <Service/>
             <Schedule/>
-        </div>
+        </section>
     )
 }
 
